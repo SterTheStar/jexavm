b6d55ef71548b89d006b54c6f2a9da55e56d5b94
Esther <151816213+SterTheStar@users.noreply.github.com>
2025-08-23 00:10:58 -0300

    added: arm64 support

--- start.py
+++ start.py
@@ -1,14 +1,16 @@
 import os
 import subprocess
 
-# Caminhos dos scripts
+# Caminhos dos scripts/binaries
 install_script = "./install.sh"
 proot_binary = "./libraries/proot"
+proot_arm64_binary = "./libraries/prootarm64"
 startup_script = "./start.sh"  # ajuste conforme seu script real
 
 # Dar permissão de execução
 os.chmod(install_script, 0o755)
 os.chmod(proot_binary, 0o755)
+os.chmod(proot_arm64_binary, 0o755)
 
 # Configurar variável de ambiente
 env = os.environ.copy()
