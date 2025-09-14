5afb4ff2d33548aa40db26cfd410d0c7cb6c4bc8
Esther <151816213+SterTheStar@users.noreply.github.com>
2025-09-14 17:54:21 -0300

    Update start.js

--- start.js
+++ start.js
@@ -15,7 +15,6 @@ console.log("Permissões alteradas, iniciando o install.sh...");
 
 // Executar o install.sh em tempo real
 const child = spawn("bash", [installScript], {
-  env: { ...process.env, STARTUPSCRIPT: startupScript },
   stdio: "inherit" // exibe stdout e stderr em tempo real
 });
 
